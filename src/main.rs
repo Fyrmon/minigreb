8dd3b4f632e545756b77157d5e2e51583411c18f
Fyrmon Warslay <fyrmonwarslay@gmail.com>
2025-11-18 16:53:37 +0100

    Change the function to build and implement it into the Config struct. Add basic error handling

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
 use std::env;
 use std::fs;
+use std::process;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = parse_config(&args);
+    let config = Config::build(&args).unwrap_or_else(|err|{
+        println!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
 
     let file_content = fs::read_to_string(config.file_name)
         .expect("File cant open/read");
@@ -19,10 +23,18 @@ struct Config
     file_name: String
 }
 
-fn parse_config(args: &[String]) -> Config
+impl Config 
 {
-    Config { 
-        _query:args[1].clone(), 
-        file_name: args[2].clone()
-    }
+    fn build(args: &[String]) -> Result<Config, &'static str>
+    {
+        if args.len() < 3 {
+            return Err("Not enough arguments for program to run");
+        }
+
+        Ok(Config { 
+            _query:args[1].clone(), 
+            file_name: args[2].clone()
+        })
+    }    
 }
+
